6ae4e1a3cf533c31ff7b74c3e19971d68a651ea1
ujjwaltiwariHp <ujjwaltiwari.hp@gmail.com>
2025-08-07 15:52:55 +0530

    Added guarantee of client delivery

--- models/messageModel.js
+++ models/messageModel.js
@@ -1,21 +1,36 @@
 const pool = require('../config/db');
 
-async function saveMessage(content) {
-  const result = await pool.query(
-    'INSERT INTO messages (content) VALUES ($1) RETURNING *',
-    [content]
-  );
-  return result.rows[0];
+async function saveMessage(content, clientOffset) {
+  try {
+    const result = await pool.query(
+      `INSERT INTO messages (content, client_offset)
+       VALUES ($1, $2)
+       ON CONFLICT (client_offset) DO NOTHING
+       RETURNING id`,
+      [content, clientOffset]
+    );
+    return result.rows[0]?.id; 
+  } catch (err) {
+    throw err;
+  }
 }
-
 async function getRecentMessages() {
   const result = await pool.query(
-    'SELECT * FROM messages ORDER BY timestamp DESC LIMIT 10'
+    'SELECT id, content FROM messages ORDER BY id DESC LIMIT 10'
   );
   return result.rows.reverse(); 
 }
 
+async function getMessagesAfter(offset) {
+  const result = await pool.query(
+    'SELECT id, content FROM messages WHERE id > $1 ORDER BY id ASC',
+    [offset]
+  );
+  return result.rows;
+}
+
 module.exports = {
   saveMessage,
-  getRecentMessages
+  getRecentMessages,
+  getMessagesAfter,
 };
--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
-const { Server }= require('socket.io');
+const { Server } = require('socket.io');
 require('dotenv').config();
 
-const { saveMessage } = require('./models/messageModel'); 
-const { getRecentMessages, recentMessages } = require('./models/messageModel');
+const { saveMessage, getRecentMessages, getMessagesAfter } = require('./models/messageModel');
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server,{
-     connectionStateRecovery: {}
+const io = new Server(server, {
+  connectionStateRecovery: {},
 });
 
 app.use(express.static(join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, './public/index.html'));
+});
 
 app.get('/recent_messages', async (req, res) => {
   try {
@@ -25,38 +28,42 @@ app.get('/recent_messages', async (req, res) => {
   }
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname,'./public/index.html'));
-});
-
 io.on('connection', async (socket) => {
   console.log('a user connected');
+  if (!socket.recovered) {
+    const serverOffset = socket.handshake.auth.serverOffset || 0;
+    try {
+      const missedMessages = await getMessagesAfter(serverOffset);
+      missedMessages.forEach((msg) => {
+        socket.emit('chat message', msg.content, msg.id);
+      });
+    } catch (err) {
+      console.error('Error recovering messages:', err);
+    }
+  }
+
+  socket.on('chat message', async (msg, clientOffset, callback) => {
+    try {
+      const insertedId = await saveMessage(msg, clientOffset);
+      if (insertedId !== undefined) {
+        io.emit('chat message', msg, insertedId);
+      }
+      callback(); 
+    } catch (err) {
+      console.error('Error saving message to DB:', err);
+      callback(); 
+    }
+  });
+
+  socket.on('disconnect', (reason) => {
+    console.log(`User disconnected. Reason: ${reason}`);
+  });
 
   socket.on('error', (err) => {
     console.error('Socket error:', err);
   });
-  try {
-    await getRecentMessages();
-    socket.emit('recent messages', recentMessages);
-  } catch (err) {
-    console.error('Error fetching recent messages:', err);
-    socket.emit('error', 'Could not fetch recent messages');
-  }
-  socket.on('chat message', async (msg) => {
-    console.log('message:', msg);
-    try {
-      await saveMessage(msg);
-      io.emit('chat message', msg);
-    } catch (err) {
-      console.error('Error saving message to DB:', err);
-      socket.emit('error', 'Could not save message');
-    }
-  });
-  socket.on('disconnect', (reason) => {
-    console.log(`User disconnected. Reason: ${reason}`);
-  });
 });
 
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+  console.log('Server running at http://localhost:3000');
 });
