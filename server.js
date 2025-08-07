7d66bd85c6869d7435f71a6c6c32d79d5ecc0ae7
ujjwaltiwariHp <ujjwaltiwari.hp@gmail.com>
2025-08-07 12:09:26 +0530

    last 10 message display to new user

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { Server }= require('socket.io');
 require('dotenv').config();
 
 const { saveMessage } = require('./models/messageModel'); 
+const { getRecentMessages, recentMessages } = require('./models/messageModel');
 
 const app = express();
 const server = createServer(app);
@@ -12,12 +13,31 @@ const io = new Server(server,{
      connectionStateRecovery: {}
 });
 
+app.use(express.static(join(__dirname, 'public')));
+
+app.get('/recent_messages', async (req, res) => {
+  try {
+    const messages = await getRecentMessages();
+    res.json(messages);
+  } catch (err) {
+    console.error('Error getting recent messages:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname,'./public/index.html'));
 });
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
   console.log('a user connected');
+     try {
+    await getRecentMessages();
+    socket.emit('recent messages', recentMessages);
+  } catch (err) {
+    console.error('Error fetching recent messages:', err);
+  }
+
   socket.on('chat message', async (msg) => {
     console.log('message: ' + msg);
 
